b19364cb8a07e3351e977efbd7552729861320ed
awura-afram <awura.afram@ashesi.edu.gh>
2022-12-07 21:14:46 +0000

    Update db_credentials.php

--- settings/db_credentials.php
+++ settings/db_credentials.php
@@ -3,3 +3,21 @@ define('SERVER', 'localhost');
 define('USERNAME', 'root');
 define('DATABASE', 'goinside');
 define('PASSWORD', getenv('MYSQLPASS') ?? '');
+
+
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USERNAME', 'bbb1d9e18d5b7a');
+define('DATABASE', 'heroku_9b3cecda316769b');
+define('PASSWORD', getenv('MYSQLPASS') ?? 'd751a7f0');
+
+// $i++;
+// $cfg["Servers"][$i]["host"] = "us-cdbr-east-06.cleardb.net"; //provide hostname
+// $cfg["Servers"][$i]["user"] = "bbb1d9e18d5b7a"; //user name for your remote ser$
+// $cfg["Servers"][$i]["password"] = "d751a7f0"; //password
+// $cfg["Servers"][$i]["auth_type"] = "config";
+// /**
+//  * Directories for saving/loading files from server
+//  */
+// $cfg['UploadDir'] = '';
+// $cfg['SaveDir'] = '';
+?>
