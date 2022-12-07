e09db81ddaa2fcf08e10eabd3e1efbd2841a5953
awura-afram <95939422+awura-afram@users.noreply.github.com>
2022-12-07 21:27:35 +0000

    Update db_credentials.php

--- settings/db_credentials.php
+++ settings/db_credentials.php
@@ -3,7 +3,7 @@
 define('SERVER', 'us-cdbr-east-06.cleardb.net');
 define('USERNAME', 'bbb1d9e18d5b7a');
 define('DATABASE', 'heroku_9b3cecda316769b');
-define('PASSWORD', getenv('MYSQLPASS') ?? 'd751a7f0');
+define('PASSWORD', 'd751a7f0');
 
 
 ?>
